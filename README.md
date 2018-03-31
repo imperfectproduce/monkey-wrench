e690fa5810c9838c082f96652be6074ba48da485
Nichole Frey <nichole@imperfectproduce.com>
2018-03-31 11:10:24 -0400

    Scope the module

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # monkey-wrench
-A collection of shared JavaScript for our node microservices, admin tooling, and the website.  
+JS :monkey: :wrench:  
--- index.spec.js
+++ index.spec.js
@@ -1,6 +1,7 @@
 import { connected } from './ui-global';
 import assert from 'assert';
 
+// Just testing that mocha is hooked up
 describe('Imports Work', () => {
   it('connected function was imported', () => {
     assert(connected);
