3a6e35925fa08da5c7195c7025ae73660828ae7c
Jesse Bibee <jesse@imperfectproduce.com>
2019-03-26 08:47:09 -0700

    Don't try to prepend queue env

--- rmq/httpApi.js
+++ rmq/httpApi.js
@@ -1,33 +1,21 @@
 const fetch = require('isomorphic-fetch');
 
-const RMQ_ENVS = [
-  'debug',
-  'dev',
-  'prod'
-];
 const VHOST = process.env.RMQ_VHOST;
 const baseUrl = `https://${VHOST}:${process.env.RMQ_PASSWORD}@${process.env.RMQ_HOST}/api`;
 
 /**
- * If the queue name is fully formed with the env, it will be returned as is.
- * Else, it prepends the RMQ_ENVIRONMENT to the queue name.
+ * Fetches the queue entity from RMQ
+ * @param {string} queueName Fully qualified queue name including env prefix
+ * @param {object} options
  */
-const getQueueNameWithEnv = (queue) => {
-  // leave it alone if already prefixed
-  if (RMQ_ENVS.some(env => queue.startsWith(`${env}.`))) return queue;
-
-  // else prepend base on RMQ_ENVIRONMENT, with fallback to debug
-  return `${process.env.RMQ_ENVIRONMENT || 'debug'}.${queue}`;
-};
-
-const getQueue = (queue, options = {}) => {
+const getQueue = (queueName, options = {}) => {
   const {
     lengthsAge = 60, // 1 minute
     lengthsIncr = 1 // 1 second
   } = options;
 
   const query = `lengths_age=${lengthsAge}&lengths_incr=${lengthsIncr}`;
-  const url = `${baseUrl}/queues/${VHOST}/${getQueueNameWithEnv(queue)}?${query}`;
+  const url = `${baseUrl}/queues/${VHOST}/${queueName}?${query}`;
 
   return fetch(url)
     .then(response => response.json());
