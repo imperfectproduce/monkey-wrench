44c0398f7d0f07e9dd73ff6908e20622265a876f
Tim Farley <tim@imperfectproduce.com>
2019-08-16 15:40:36 -0600

    Explicit export default.

--- ui-global/connected.js
+++ ui-global/connected.js
@@ -1,21 +1,17 @@
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
-/*
- * Simplifies the connect & bindActionCreators for react components.
- */
-export default (mapStateToProps, actions) => Component => {
+const connected = (mapStateToProps, actions) => Component => {
   const mapDispatchToProps = (dispatch, ownProps) => {
     if (actions) {
       return {
-        ...ownProps,
         actions: {
           ...ownProps.actions,
           ...bindActionCreators(actions, dispatch)
         }
       };
     }
-    return ownProps;
+    return null;
   };
 
   return connect(
@@ -23,3 +19,5 @@ export default (mapStateToProps, actions) => Component => {
     mapDispatchToProps
   )(Component);
 };
+
+export default connected;
