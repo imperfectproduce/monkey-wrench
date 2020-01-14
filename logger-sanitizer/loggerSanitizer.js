7fdd1e0554eaeae674a934a1871cd43959ab03fd
Sar Perlman <sar.perlman@imperfectproduce.com>
2020-01-13 20:52:43 -0800

    Add global logger-sanitizer module for use in node services

---
+++ logger-sanitizer/loggerSanitizer.js
@@ -0,0 +1,36 @@
+import uniq from 'lodash/uniq';
+import _isArray from 'lodash/isArray';
+import _isPlainObject from 'lodash/isPlainObject';
+
+// Replaces password info in a logging message with ['redacted']
+const loggerSanitizer = (data) => {
+  
+  // define list of keys to sanitize
+  const blacklistedLogKeys = ['pass', 'password', 'pw', 'key', 'token'];
+  
+  // handle arrays
+  if (_isArray(data)) {
+    return data.map(item => loggerSanitizer(item, blacklistedLogKeys));
+  }
+
+  // handle non-objects
+  if (!_isPlainObject(data)) return data;
+
+  // handle objects
+  return Object.keys(data).reduce((acc, key) => {
+    /* eslint-disable no-param-reassign */
+    if (blacklistedLogKeys.includes(key.toLowerCase())) {
+    acc[key] = '[redacted]';
+  } else {
+    if (typeof data[key] === 'object' && data[key] !== null) {
+      acc[key] = loggerSanitizer(data[key], blacklistedLogKeys);
+    } else {
+      acc[key] = data[key];
+    }
+  }
+  /* eslint-enable no-param-reassign */
+  return acc;
+}, {});
+};
+
+export default loggerSanitizer;
---
+++ logger-sanitizer/loggerSanitizer.spec.js
@@ -0,0 +1,54 @@
+/* eslint-env mocha */
+import expect from 'expect';
+import loggerSanitizer from './loggerSanitizer';
+
+describe('Logger messages sanitizer redacts blacklisted passwords, tokens and keys', () => {
+  it('Should redact values for all blacklisted (sensitive) keys from log message', () => {
+    const testLog = {
+      user: 'tester',
+      password: '12345',
+      data: {
+        pass: '12345',
+      },
+      key: '234567',
+      token: '345678',
+      happyKey: 'I am a happy key'
+    };
+    const data = loggerSanitizer(testLog);
+    expect(data.password).toEqual('[redacted]');
+    expect(data.data.pass).toEqual('[redacted]');
+    expect(data.key).toEqual('[redacted]');
+    expect(data.token).toEqual('[redacted]');
+  });
+  it('Should not redact values for keys not on blacklisted list', () => {
+    const testLog = {
+      user: 'tester',
+      password: '12345',
+      token: '345678',
+      happyKey: 'I am a happy key'
+    };
+    const data = loggerSanitizer(testLog);
+    expect(data.user).toEqual('tester');
+    expect(data.happyKey).toEqual('I am a happy key');
+  });
+  it('Should not redact a string', () => {
+    const testLog = 'happyString';
+    const data = loggerSanitizer(testLog);
+    expect(data).toEqual('happyString');
+  });
+  it('Should not filter arrays unless the array value is an object', () => {
+    const testLog = [
+      'happyValue',
+      { user: 'tester', password: '12345' },
+      [
+        { user: 'tester2', pass: '123456' },
+        'happyValue2'
+      ]
+    ];
+    const data = loggerSanitizer(testLog);
+    expect(data[0]).toEqual('happyValue');
+    expect(data[1].password).toEqual('[redacted]');
+    expect(data[2][0].pass).toEqual('[redacted]');
+    expect(data[2][1]).toEqual('happyValue2');
+  });
+});
